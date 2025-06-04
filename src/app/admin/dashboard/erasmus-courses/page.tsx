15135a7ba8db4bd1a2d841066bf362a6da1f4ba0
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-06-05 00:49:39 +0100

    affichage page erasmus courses

--- src/app/admin/dashboard/erasmus-courses/page.tsx
+++ src/app/admin/dashboard/erasmus-courses/page.tsx
@@ -9,7 +9,7 @@ import ErasmusCourseTable from "@/components/erasmus/erasmus-course-table";
 import Pagination from "@/components/ui/pagination";
 
 export const metadata: Metadata = {
-  title: 'Erasmus Courses',
+  title: "Erasmus Courses",
 };
 
 export default async function Page(props: {
@@ -18,26 +18,26 @@ export default async function Page(props: {
     page?: string;
   }>;
 }) {
-      const searchParams = await props.searchParams;
-      const query = searchParams?.query || '';
-      const currentPage = Number(searchParams?.page) || 1;
-      const totalPages = await fetchErasmusCoursesPages(query);
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchErasmusCoursesPages(query);
 
-    return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-            <h1 className="text-2xl font-bold">Erasmus Courses</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-              <Search placeholder="Pesquisar courses..." />
-              <CreateErasmusCourse />
-            </div>
-            <Suspense key={query + currentPage} fallback={<ErasmusCourseSkeleton />}>
-                <ErasmusCourseTable query={query} currentPage={currentPage} />
-            </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-              <Pagination totalPages={totalPages} />
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Erasmus Courses</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Pesquisar courses..." />
+        <CreateErasmusCourse />
+      </div>
+      <Suspense key={query + currentPage} fallback={<ErasmusCourseSkeleton />}>
+        <ErasmusCourseTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
